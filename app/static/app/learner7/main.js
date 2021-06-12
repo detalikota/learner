f7f19f02e7587c43c59804d2bdbe3e70c4e90594
detalikota <detalikota@gmail.com>
2021-06-12 23:08:17 +0300

    yeah

--- app/static/app/learner7/main.js
+++ app/static/app/learner7/main.js
@@ -1,13 +1,19 @@
+window.onload = appear();
 var start;
 var clicked;
+var reactionTime;
+var numbers = [1000,2000,3000,4000];
+var randomNumber = Math.floor(Math.random() * 3);
+var number = numbers[randomNumber];
 function disappear(){
     document.getElementById("box").style.display='none';
     clicked = Date.now();
-    var reactionTime = clicked-start;
-    setTimeout(appear,1000);
-}
+    reactionTime = (clicked-start)/1000;
+    setTimeout(appear,number);
+    alert('your reaction time is'+ reactionTime);
+} 
 function appear(){
     document.getElementById('box').style.display='block';
     start = Date.now();
 }
-document.getElementById("result").innerHTML=("Your resul is: "+(appear()-disappear()));
+/* document.getElementById("result").innerHTML=(disappear()); */
