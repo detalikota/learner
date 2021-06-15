c8d1e27584fdd1851a7279b536cb00d70691d7da
detalikota <detalikota@gmail.com>
2021-06-15 17:48:38 +0300

    yeah

--- app/static/app/learner7/main.js
+++ app/static/app/learner7/main.js
@@ -13,6 +13,18 @@ function disappear(){
     alert('your reaction time is'+ reactionTime);
 } 
 function appear(){
+    var randomTop = Math.random()*400;
+    var randomLeft = Math.random()*1000;
+    var randomHeight = (Math.random()*200)+50;
+    var randomWidth = (Math.random()*200)+50;
+    var randomCurve = Math.random();
+    if (randomCurve < 0.5) {
+        document.getElementById('box').style.borderRadius=('500px');
+    }
+    document.getElementById('box').style.height=(randomHeight+'px');
+    document.getElementById('box').style.width=(randomWidth+'px');
+    document.getElementById('box').style.top=(randomTop+'px');
+    document.getElementById('box').style.left=(randomLeft+'px');
     document.getElementById('box').style.display='block';
     start = Date.now();
 }
